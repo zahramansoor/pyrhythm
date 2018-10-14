4554cb8a3284015a3e2d1578d344a72745ae6ad8
Zahra <zahramansoor@github.com>
2018-10-14 10:34:39 -0400

    autopush

--- fem.py
+++ fem.py
@@ -9,7 +9,6 @@ Created on Mon Sep 17 10:36:52 2018
 ################This version treats only the element nodes in the FHN element as FHN nodes################
 
 import sys, numpy as np
-import matplotlib.pyplot as plt
 from element import element 
 from functions import global_to_element_21x21, r_FHN, residual_phi_FHN, partial_derivative_phi_R_phi_FHN, element_to_global_21x21
 from initialisation import fill_params, fill_constants
@@ -19,32 +18,32 @@ def run_rhythm(params, constants, verbose = False):
     
     for n in constants['time']:
     
-        #initalise things
+        #initalise/update things
         if n != 0:
             phi_global = params['phi_global_time'][:, (n - 1)] #using the 'passed' phi_global frm previous time
         else:
             phi_global = params['phi_global']
-            params['r'][n, :] = params['r'][(n - 1), :]
-            
-        R_phi_global = params['R_phi_global'];  Tol = params['R_phi_global']; phi_step = params['phi_step']
-        phi = params['phi']
-        
+                                  
         sys.stdout.write('\n\n***************************************************************     Starting iterations for n = {}\n\n'.format(n)); sys.stdout.flush()
         
         Tol = np.ones((global_matrix_size, 1))*constants['tol'] #using global matrix size
-    
-        while sum(abs(R_phi_global)) > sum(Tol):
+        
+        #re-initialise R_phi_global to start while loop
+        params['R_phi_global'] = np.ones(global_matrix_size)
+        
+        while sum(abs(params['R_phi_global'])) > sum(Tol):
                     
-            phi_step[n] += 1
+            params['phi_step'][n] += 1
             
             if verbose: sys.stdout.write('\n     Redistributing the phi_global the elements for element-level calculations\n\n'); sys.stdout.flush()
-    
-            params['phi'][n,:] = global_to_element_21x21(np.zeros(1764), phi_global)
             
+            #redistribute/initialise things
+            params['phi'][n,:] = global_to_element_21x21(np.zeros(1764), phi_global)
+            params['r'][n, :] = params['r'][(n - 1), :]
+        
             for z_d in np.arange(0, 1761, 4):
                 nodes = [z_d, (z_d + 1), (z_d + 2), (z_d + 3)] ##assign node values & print variable
-                params = element(nodes, n, params, constants)
-                
+                params = element(nodes, n, params, constants)                
                 if verbose: sys.stdout.write('\n     Nodes: {}\n\n    Residuals: {}\n\n'.format(nodes, params['R_phi'][z_d:(z_d+4)])); sys.stdout.flush()
                 
                 #FHN element
@@ -52,32 +51,27 @@ def run_rhythm(params, constants, verbose = False):
                     nodes = [z_d, (z_d + 1), (z_d + 2), (z_d + 3)]
                     k = min(nodes)
                     
-                    #initalise things
-                    r = params['r']; R_phi = params['R_phi']; phi = params['phi']; p_phi_R_phi = params['p_phi_R_phi']
+                    #initalise things to feed into functions
+                    r = params['r']; phi = params['phi']; 
                     
                     for j in [0,1,2,3]:
-                        r[n, (j + k)] = r_FHN(r[(n - 1), (j + k)], phi[n, (j + k)], **constants)
-                        params['r'] = r
-                        R_phi[j + k] = residual_phi_FHN(j, phi, r, nodes, n, **constants)
-                        params['R_phi'] = R_phi
+                        params['r'][n, (j + k)] = r_FHN(r[(n - 1), (j + k)], phi[n, (j + k)], **constants)
+                        params['R_phi'][j + k] = residual_phi_FHN(j, phi, r, nodes, n, **constants)
                         
                         #partial derivative matrix
                         for m in [0,1,2,3]:
-                            p_phi_R_phi[(m + k), (j + k)] = partial_derivative_phi_R_phi_FHN(j, m, phi, r, nodes, n, **constants)
-                            params['p_phi_R_phi'] = p_phi_R_phi
-                            
-                params['phi'] = phi
-                    
+                            params['p_phi_R_phi'][(m + k), (j + k)] = partial_derivative_phi_R_phi_FHN(j, m, phi, r, nodes, n, **constants)
+                                                
                 if verbose: sys.stdout.write('\n     Nodes: {}\n\n    Residuals: {}\n\n'.format(nodes, params['R_phi'][z_d:(z_d + 4)])); sys.stdout.flush()
     
             if verbose: sys.stdout.write('\n\n     Assembling 1764 element nodes to 484 global nodes...\n\n'); sys.stdout.flush()
             
-            R_phi_global, p_phi_R_phi_global = element_to_global_21x21(params['R_phi'], params['p_phi_R_phi'])
+            params['R_phi_global'], params['p_phi_R_phi_global'] = element_to_global_21x21(params['R_phi'], params['p_phi_R_phi'])
             
             #Newton's iteration for phi
-            phi_global = phi_global - np.matmul(np.linalg.inv(p_phi_R_phi_global), R_phi_global)
+            phi_global = phi_global - np.matmul(np.linalg.inv(params['p_phi_R_phi_global']), params['R_phi_global'])
             
-            sys.stdout.write('\n\n     Maximum value of residual for n = {}: {}\n'.format(n, max(abs(R_phi_global)))); sys.stdout.flush()
+            sys.stdout.write('\n\n     Maximum value of residual for n = {}: {}\n'.format(n, max(abs(params['R_phi_global'])))); sys.stdout.flush()
     
             params['phi_global_time'][:, n] = phi_global #updates global phi matrix for time loop
         
